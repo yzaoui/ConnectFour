f696b40909a862c33dd2d74525687b4a9c56a651
Y Za <yzaoui7@gmail.com>
2016-12-06 20:57:54 -0500

    Remove try-catch until proper exception handling

--- src/main.cc
+++ src/main.cc
@@ -46,86 +46,82 @@ void close() {
 }
 
 int main(int argc, char** argv) {
-	try {
-		init();
+	init();
 
-		bool quit = false;
-		SDL_Event e;
+	bool quit = false;
+	SDL_Event e;
 
-		GameBoard board;
+	GameBoard board;
 
-		CFTexture boxEmpty(renderer);
-		boxEmpty.loadFromFile("C4Empty.png");
-		CFTexture boxYellow(renderer);
-		boxYellow.loadFromFile("C4Yellow.png");
-		CFTexture boxRed(renderer);
-		boxRed.loadFromFile("C4Red.png");
+	CFTexture boxEmpty(renderer);
+	boxEmpty.loadFromFile("C4Empty.png");
+	CFTexture boxYellow(renderer);
+	boxYellow.loadFromFile("C4Yellow.png");
+	CFTexture boxRed(renderer);
+	boxRed.loadFromFile("C4Red.png");
 
-		const int BOX_SIZE = boxEmpty.getWidth();
-		const int NUM_BOXES_HORIZONTAL = board.getBoxesHorizontal();
-		const int NUM_BOXES_VERTICAL = board.getBoxesVertical();
-		const int BOARD_OFFSET_WIDTH = (SCREEN_WIDTH - BOX_SIZE * NUM_BOXES_HORIZONTAL) / 2;
-		const int BOARD_OFFSET_HEIGHT = (SCREEN_HEIGHT - BOX_SIZE * NUM_BOXES_VERTICAL) / 2;
+	const int BOX_SIZE = boxEmpty.getWidth();
+	const int NUM_BOXES_HORIZONTAL = board.getBoxesHorizontal();
+	const int NUM_BOXES_VERTICAL = board.getBoxesVertical();
+	const int BOARD_OFFSET_WIDTH = (SCREEN_WIDTH - BOX_SIZE * NUM_BOXES_HORIZONTAL) / 2;
+	const int BOARD_OFFSET_HEIGHT = (SCREEN_HEIGHT - BOX_SIZE * NUM_BOXES_VERTICAL) / 2;
 
-		uint8_t frameTime;
+	uint8_t frameTime;
 
-		while (!quit) {
-			frameTime = SDL_GetTicks();
+	while (!quit) {
+		frameTime = SDL_GetTicks();
 
-			/***************
-			 * INPUT
-			 ***************/
+		/***************
+		 * INPUT
+		 ***************/
 
-			while (SDL_PollEvent(&e) != 0) {
-				if (e.type == SDL_QUIT ||
-					(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)) {
-					quit = true;
-				} else if (e.type == SDL_MOUSEBUTTONDOWN) {
-					int x;
-					SDL_GetMouseState(&x, nullptr);
+		while (SDL_PollEvent(&e) != 0) {
+			if (e.type == SDL_QUIT ||
+				(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)) {
+				quit = true;
+			} else if (e.type == SDL_MOUSEBUTTONDOWN) {
+				int x;
+				SDL_GetMouseState(&x, nullptr);
 
-					if (x > BOARD_OFFSET_WIDTH && x < SCREEN_WIDTH - BOARD_OFFSET_WIDTH) {
-						board.play((x - BOARD_OFFSET_WIDTH) / BOX_SIZE);
-					}
+				if (x > BOARD_OFFSET_WIDTH && x < SCREEN_WIDTH - BOARD_OFFSET_WIDTH) {
+					board.play((x - BOARD_OFFSET_WIDTH) / BOX_SIZE);
 				}
-			}
-
-			/***************
-			 * RENDERING
-			 ***************/
-
-			renderer->clear();
-
-			for (int x = 0; x < NUM_BOXES_HORIZONTAL; x++) {
-				for (int y = 0; y < NUM_BOXES_VERTICAL; y++) {
-					switch (board.getBox(x, y)) {
-						case Box::empty:
-							boxEmpty.render(BOARD_OFFSET_WIDTH + x * BOX_SIZE, BOARD_OFFSET_HEIGHT + y * BOX_SIZE);
-							break;
-						case Box::yellow:
-							boxYellow.render(BOARD_OFFSET_WIDTH + x * BOX_SIZE, BOARD_OFFSET_HEIGHT + y * BOX_SIZE);
-							break;
-						case Box::red:
-							boxRed.render(BOARD_OFFSET_WIDTH + x * BOX_SIZE, BOARD_OFFSET_HEIGHT + y * BOX_SIZE);
-							break;
-					}
-				}
-			}
-
-			renderer->present();
-
-			/***************
-			 * TIMING
-			 ***************/
-
-			int ticks = SDL_GetTicks() - frameTime;
-
-			if (ticks < 1000.0 / 60.0) {
-				SDL_Delay(1000.0 / 60.0 - ticks);
 			}
 		}
-	} catch (const char*& e) {
-		std::cout << e << std::endl;
+
+		/***************
+		 * RENDERING
+		 ***************/
+
+		renderer->clear();
+
+		for (int x = 0; x < NUM_BOXES_HORIZONTAL; x++) {
+			for (int y = 0; y < NUM_BOXES_VERTICAL; y++) {
+				switch (board.getBox(x, y)) {
+					case Box::empty:
+						boxEmpty.render(BOARD_OFFSET_WIDTH + x * BOX_SIZE, BOARD_OFFSET_HEIGHT + y * BOX_SIZE);
+						break;
+					case Box::yellow:
+						boxYellow.render(BOARD_OFFSET_WIDTH + x * BOX_SIZE, BOARD_OFFSET_HEIGHT + y * BOX_SIZE);
+						break;
+					case Box::red:
+						boxRed.render(BOARD_OFFSET_WIDTH + x * BOX_SIZE, BOARD_OFFSET_HEIGHT + y * BOX_SIZE);
+						break;
+				}
+			}
+		}
+
+		renderer->present();
+
+		/***************
+		 * TIMING
+		 ***************/
+
+		int ticks = SDL_GetTicks() - frameTime;
+
+		if (ticks < 1000.0 / 60.0) {
+			SDL_Delay(1000.0 / 60.0 - ticks);
+		}
 	}
 
 	close();
